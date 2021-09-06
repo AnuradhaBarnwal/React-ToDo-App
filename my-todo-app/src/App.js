8428447b1af8b7d53c946dbc1cb1e04237fb61a1
AnuradhaBarnwal <anu.barnwal24@gmail.com>
2021-09-06 10:53:58 +0530

    add task done

--- my-todo-app/src/App.js
+++ my-todo-app/src/App.js
@@ -1,11 +1,69 @@
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import Tasks from "./Tasks";
 
-function App() {
-  return (
-    <div className="App">
-     <h1>Today's Plan</h1>
-    </div>
-  );
+class App extends Component {
+
+  state = {
+    items:[],
+    text:""
+  }
+
+  handleChange = e => {
+    this.setState({ text: e.target.value})
+  }
+
+  handleAdd = e => {
+    if (this.state.text !== ""){
+      const items = [...this.state.items , this.state.text];
+      this.setState({items : items, text: ""})
+    }
+  }
+
+  handleDelete = id => {
+    console.log("Deleted", id)
+    const allItems = [...this.state.items];
+    console.log("allItems", allItems);
+    const items = allItems.filter((element,i) => {
+      return i !== id;
+    })
+    console.log("Items", items)
+    this.setState({ item: items});
+  }
+
+  render() {
+    return (
+      <div className="container-fluid my-5">
+        <div className="row">
+          <div className="col-sm-6 mx-auto text-black-50 shadow-lg p-3">
+            <h1 className="text-center">Today's Plan</h1>
+            <div className="row">
+              <div className="col-9">
+                <input type="text" className="form-control" placeholder="today's task" value={this.state.text} onChange={this.handleChange}/>
+              </div>
+              <div className="col-2">
+                <button className="btn btn-warning px-5 font-weight-bold" onClick={this.handleAdd}>Add</button>
+              </div>
+              <div className="container-fluid">
+                {/* <ul className="list-unstyled row m-5">
+                {
+                  this.state.items.map((value,i) => {
+                    return <Tasks key={i} id={i} value={value} sendData={this.handleDelete} />
+                  })
+                }
+                  
+                </ul> */}
+                <ul className="list-unstyled row m-5">
+                  <Tasks p={this.state.items} sendData={this.handleDelete}/>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
